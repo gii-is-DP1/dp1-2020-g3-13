d1b1180d38b012c796af7de08adc60fbcab0aa51
Alejandro Baños Gonzalez <alebangon@alum.us.es>
2020-11-15 19:47:35 +0100

    Creación controlador y vista admin
    Creados el controlador y la vista de admin ademas de insertar uno en la base de datos y una ultima corrección a la entidad persona

--- src/main/java/org/springframework/samples/petclinic/service/AdminService.java
+++ src/main/java/org/springframework/samples/petclinic/service/AdminService.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Admin;
 import org.springframework.samples.petclinic.repository.AdminRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,5 +14,9 @@ public class AdminService {
     @Transactional
     public int adminCount(){
         return (int) adminRepo.count();
+    }
+
+    public Iterable<Admin> findAll(){
+        return adminRepo.findAll();
     }
 }
