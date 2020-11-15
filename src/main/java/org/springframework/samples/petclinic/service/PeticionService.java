99b20bf5e44711bcb71506644d53972a47e076c7
Romanflodriguez <Romanflodriguez13@gmail.com>
2020-11-15 16:37:56 +0100

    Creation of entity petition
    class
    repository
    service
    test

---
+++ src/main/java/org/springframework/samples/petclinic/service/PeticionService.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.repository.PeticionRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PeticionService {
+    @Autowired
+    private PeticionRepository peticionRepo;
+
+    @Transactional
+    public int peticionCount(){
+    return (int) peticionRepo.count();
+    }
+    
+}
