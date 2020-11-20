1cc47dec6e13e97c249dea1dc0da4a6d8f6295e6
Ezequiel <Ezequielperez50@gmail.com>
2020-11-20 17:38:36 +0100

    añadir consulta añadido

--- src/main/java/org/springframework/samples/petclinic/web/PeticionController.java
+++ src/main/java/org/springframework/samples/petclinic/web/PeticionController.java
@@ -1,13 +1,19 @@
 package org.springframework.samples.petclinic.web;
 
 import java.util.Collection;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Peticion;
 import org.springframework.samples.petclinic.service.PeticionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //www.paco.es/peticiones/listado 
 
@@ -16,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/peticion")//seria la pagina donde estan todas las peticiones
 public class PeticionController {
+
+    private static final String VIEWS_CREATE_FORM = "peticion/CreatePeticionForm";
+
     @Autowired
     private PeticionService peticionServ;
     @GetMapping()
@@ -26,5 +35,26 @@ public class PeticionController {
         return vista;
 
     }
+
+    @GetMapping(value="/new")
+    public String initCreationForm(Map<String, Object> model){
+        Peticion peticion = new Peticion();
+        model.put("peticion", peticion);
+		return VIEWS_CREATE_FORM;
+
+    }
+    @PostMapping(value = "/new")
+    public String processCreationForm(@Valid Peticion peticion, BindingResult result){
+        if(result.hasErrors()){
+            return VIEWS_CREATE_FORM;
+        }else{
+            this.peticionServ.saveOrganizacion(peticion);
+            return "redirect:/"; 
+            //TODO Revisar
+        }
+
+    }
+
+    
     
 }
