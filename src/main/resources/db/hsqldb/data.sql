d5420e3eee66ac0d50fcb147d992a9fb79283fad
Romanflodriguez <Romanflodriguez13@gmail.com>
2020-11-23 16:24:03 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -73,7 +73,10 @@ INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04',
 
 
 -- INSERT INTO organizaciones(id, cif, email, info, password, usuario, nombre_organizacion,fk_peticion) VALUES (1,'690321145', 'antonio@elbola.es','EMPRESA DE BALONES CUADRADOS','pelotasCuadradas','antBola', 'BALON CUADRADO',1);
-INSERT INTO peticion(id,cif,email,info,nombre_organizacion) VALUES (1,'6969696969','PRIMOPEPE@PEPE.COM','EMPRESA DE VINO SIN ALCOHOL','PRIMO PEPE');
+INSERT INTO peticion(id,cif,email,info,nombre_organizacion) VALUES (1,'6969696969','PRIMOPEPE@paco.COM','EMPRESA DE VINO SIN ALCOHOL','PRIMO PEPE');
+INSERT INTO peticion(id,cif,email,info,nombre_organizacion) VALUES (2,'96969696','francisquito@PEPE.COM','cisquito el cantaor','francisco PEPE');
+INSERT INTO peticion(id,cif,email,info,nombre_organizacion) VALUES (3, '666666666','travis@scott.COM','conciertos de trap','travieso scotte');
+INSERT INTO peticion(id,cif,email,info,nombre_organizacion) VALUES (4,'9999999','yung@beef.COM','conciertos de trap español','ternera joven');
 
 
 INSERT INTO consulta(id, name, asunto, descripcion, fecha_consulta) VALUES(1, 'test', 'test_asunto', 'test_descripcion', '2020-03-03');
