030aeb27e3fe0eafaecd43fecbef57f76938bd04
Ezequiel <Ezequielperez50@gmail.com>
2020-11-23 12:53:24 +0100

    añadida la H3-3 Y H3-4 (parcialmente - solo eliminar)

--- src/main/java/org/springframework/samples/petclinic/repository/PeticionRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/PeticionRepository.java
@@ -2,5 +2,4 @@ package org.springframework.samples.petclinic.repository;
 import org.springframework.samples.petclinic.model.Peticion;
 import org.springframework.data.repository.CrudRepository;
 public interface PeticionRepository extends CrudRepository<Peticion, Integer>{
-    public Peticion findById(int organizacionId);
 }
