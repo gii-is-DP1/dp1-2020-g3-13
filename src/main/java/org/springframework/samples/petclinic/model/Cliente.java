97681e2c0642cc345acd8475fc1b64e1dce73542
Romanflodriguez <Romanflodriguez13@gmail.com>
2020-11-27 20:16:17 +0100

    Relaciones Entrada

--- src/main/java/org/springframework/samples/petclinic/model/Cliente.java
+++ src/main/java/org/springframework/samples/petclinic/model/Cliente.java
@@ -2,10 +2,15 @@ package org.springframework.samples.petclinic.model;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 
@@ -19,6 +24,8 @@ import lombok.Data;
 @Data
 @Table(name = "clientes")
 public class Cliente extends Persona{
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "cliente")
+   private  List<VentaEntrada> entradas;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "nombre_usuario", referencedColumnName = "nombreUsuario")
