80df4498cd1d3f14d9ea858c3d62f54cc69c1dc3
Andrea <andmecsan@alum.us.es>
2020-12-13 12:49:51 +0100

    Hecha la parte de eliminar de la H1-1
    Ademas de las modificaciones necesarias para la parte de la prueba de editas

--- src/test/java/org/springframework/samples/petclinic/service/ClienteServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClienteServiceTest.java
@@ -6,16 +6,36 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.model.Cliente;
+import org.springframework.samples.petclinic.repository.ClienteRepository;
+import org.springframework.samples.petclinic.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class ClienteServiceTest {
     @Autowired
     private ClienteService clienteService;
+    @Autowired
+    private ClienteRepository clienteRepo;
     @Test
     public void testCountWithInitialData(){
         int count= clienteService.clienteCount();
         assertEquals(count, 0);
+    }
+
+    @Test
+    public void deleteClienteTest(){
+        Integer clienteAntes = (int) clienteRepo.count();
+        clienteService.deleteCliente(clienteService.findClienteByUsuario("prueba1"));
+        Integer clienteDespues = (int) clienteRepo.count();
+        assertEquals(clienteAntes-1, clienteDespues);
+
+    }
+
+    @Test
+    public void modifyClienteTest(){
+        Cliente clienteActual = clienteService.findClienteByUsuario("prueba1");
+        
     }
     
 }
