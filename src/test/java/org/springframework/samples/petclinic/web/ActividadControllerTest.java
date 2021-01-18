68ad841152037f10a46241c9817dabbf51e9abb1
ezepersos <ezequielperez50@gmail.com>
2021-01-18 15:10:12 +0100

    Hotfix: Corregido el test de Actividad, Nuevo: Test de controllador de Actividad (incompleto)

--- src/test/java/org/springframework/samples/petclinic/web/ActividadControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/ActividadControllerTest.java
@@ -11,15 +11,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.samples.petclinic.service.ConsultaService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ActividadControllerTest {
@@ -36,7 +39,7 @@ public class ActividadControllerTest {
 
     @BeforeEach
     void setup(){
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    //    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
 
@@ -46,9 +49,10 @@ public class ActividadControllerTest {
         mockMvc.perform(get("/eventos/{evento_id}/actividades",1)).andExpect(status().isOk()).andExpect(view().name(VIEWS_ACTIVIDAD_LISTA_ACTIVIDADES)).andExpect(model().attributeExists("actividades"));
     }
 
-    @WithMockUser(value = "usuario")
+    
     @Test
     public void testFormularioActividad() throws Exception{
+
         mockMvc.perform(get("/eventos/{evento_id}/actividades/nuevo", 1)).andExpect(status().isOk()).andExpect(view().name(VIEWS_ACTIVIDAD_CREATE_OR_UPDATE_FORM)).andExpect(model().attributeExists("listaId","lugaresRealizacion","actividad"));
     }
     //Terminar
