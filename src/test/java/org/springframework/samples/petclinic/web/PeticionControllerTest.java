489c22408c05cee723845175df8f15079d51715c
julibamon <julibamon@alum.us.es>
2021-02-05 19:47:27 +0100

    arreglado test controller peticion

--- src/test/java/org/springframework/samples/petclinic/web/PeticionControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/PeticionControllerTest.java
@@ -1,47 +1,23 @@
 package org.springframework.samples.petclinic.web;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
-import org.springframework.samples.petclinic.service.AutoridadesService;
-import org.springframework.samples.petclinic.service.ClienteService;
-import org.springframework.samples.petclinic.service.ConsultaService;
-import org.springframework.samples.petclinic.service.EnvioEmailService;
-import org.springframework.samples.petclinic.service.OrganizacionService;
-import org.springframework.samples.petclinic.service.PeticionService;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PeticionControllerTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
     private MockMvc mockMvc;
 
-    private static final String VIEWS_PETICION_CREATE_FORM = "peticion/CreatePeticionForm";
-
-    // Prueba a hacer la petición POST del formulario de peticion
     @Test
     @WithMockUser
     public void testCrearPeticionFormularioExito() throws Exception {
@@ -50,5 +26,11 @@ public class PeticionControllerTest {
                 .param("info", "informacion de la organizacion"));
 
     }
+
+    // @Test
+    // @WithMockUser
+    // public void testFormularioCrearPeticion() throws Exception {
+    // mockMvc.perform(get("/peticion/new")).andExpect(status().isOk());
+    // }
 
 }
